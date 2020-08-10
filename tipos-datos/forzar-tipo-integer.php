a23bb1893e1e55328707aa41ae33f0ae206e5267
Víctor Cuervo <vcuervo@gmail.com>
2020-08-10 02:40:54 +0200

    Forzar integer

---
+++ tipos-datos/forzar-tipo-integer.php
@@ -0,0 +1,12 @@
+<?php
+
+$valor_booleano = true;
+$valor_float = 8.9;
+
+echo 'Valor booleano '.(int)$valor_booleano.'<br>';
+echo 'Valor float '.(int)$valor_float.'<br>';
+
+echo 'Valor booleano '.intval($valor_booleano).'<br>';
+echo 'Valor float '.intval($valor_float).'<br>';
+
+?>
